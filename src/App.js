12fbd02de766edbbae093abd0c15ec1b46a8ccbf
David Melo <davidmelo@desmdesigns.com>
2019-08-20 17:53:39 -0400

    First

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { useMachine } from '@xstate/react';
+import { Machine } from 'xstate';
+import * as yup from 'yup';
+
+let schema = yup.object().shape({
+  name: yup.string().required()
+});
+
+function validateContext(context) {
+  console.log(context);
+  return schema
+    .isValid(context)
+    .then(function(valid) {
+      console.log(true)
+    });
+}
+
+const toggleMachine = Machine({
+  id: 'toggle',
+  initial: 'idle',
+  context: {
+    name: null
+  },
+  states: {
+    idle: {
+      on: {
+        START: 'start'
+      }
+    },
+    start: {
+      invoke: {
+        src: validateContext,
+        onDone: {
+          target: 'active'
+        }
+      }
+    },
+    active: {
+      on: { 
+        TOGGLE: 'idle' 
+      }
+    }
+  }
+});
+
+export const Toggler = () => {
+  const [current, send] = useMachine(toggleMachine);
+
+  return (
+    <button onClick={() => send('START')}>
+      Start
+    </button>
+  );
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toggler/>
     </div>
   );
 }
